85f282f062635c62bd81b00e21677f34c19e327c
Michael Peterson <mikebean4551@gmail.com>
2021-05-28 02:02:17 -0700

    fixing table

--- static/src/Projects.js
+++ static/src/Projects.js
@@ -9,21 +9,23 @@ class Projects extends React.Component {
     render() {
         getProjects()
             .then(res => this.setState(state => {
-
                 return {projects : res.data};
-
-            }))
+           }))
             .catch(reason => console.error(reason));
 
         return (
-            <div>
-                <h2>Projects</h2>
-                <table>
+            <div className="Projects">
+                {this.state.projects && this.state.projects.length > 0 &&
+                <>
+                <h1>Projects</h1>
+                <table border="1">
                     <thead><tr><th>name</th><th>description</th><th>source code url</th></tr></thead>
                     <tbody>
                         {this.state.projects.map((d) => <tr><td>{d.name}</td><td>{d.description}</td><td>{d.source_url}</td></tr>)}
                     </tbody>
                 </table>
+                </>
+            }
             </div>
         );
     }
